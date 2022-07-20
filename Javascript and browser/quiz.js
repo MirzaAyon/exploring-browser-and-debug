0b05450d100b985e0dd23cf15938e7345b783a6a
MirzaAyon <ayonmirza98@gmail.com>
2022-07-21 01:18:12 +0600

    quiz added

---
+++ Javascript and browser/quiz.js
@@ -0,0 +1,21 @@
+//6
+// fetch().then().then.().finally()
+// (I won't tell you what to google. So, you have to think about it. )
+
+// Find out When will the finally() method be executed?
+
+// ans: when the promise is fullfilled
+// and when the promise is rejected both
+
+
+//9
+// What is a heap?
+//a last mostly unstructured region of memory
+
+//12
+// Javascript executes a script ________
+//ans : from the top of the file and works its way down
+
+//13
+//A call stack is a _______
+//a call stack is a mechanism for an interpreter
