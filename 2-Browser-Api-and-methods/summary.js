c4b857c1055b6023e93e9a3dca81f20e78c2249f
MirzaAyon <ayonmirza98@gmail.com>
2022-07-22 19:28:16 +0600

    summary added

---
+++ 2-Browser-Api-and-methods/summary.js
@@ -0,0 +1,21 @@
+//browser er mdhe api ache 
+//plain js e docu use korte parbo na but browser e use korte parbo
+//fetch o same 
+
+alert('');
+confirm('are you single?')
+prompt('name');
+
+// location.reload()
+// location.href = ''
+// location.hash =
+// http://127.0.0.1:5500/shopping.html
+
+// history.back()
+// history.go()
+
+// cookies //er madhome info web site e rakhbo
+
+// sessionStorage vs localStorage //interview
+// cookies vs sessionStorage vs localStorage //interview
+//browser web api theke clipboard er use shikhbo
---
+++ 2-Browser-Api-and-methods/summary2.js
@@ -0,0 +1,18 @@
+//first e ekta website er inspact - console e giye
+document.body.contentEditable
+'inherit'
+//tar mane inherit kora
+//now true kore dilam
+document.body.contentEditable = true
+true
+//ekhn content chaile edit kora jabe evabe content ke editable kora jae
+
+
+//chorme hacks
+//ager tab ja remove kore disi ta ber korar jnno
+//control+shift+t
+
+//new tab khular jnno - chorme://newtab
+
+//shift+esc dile ke ke memory khae ta jana jabe
+//chorme://restart dile shob tab resatrt hbe 
